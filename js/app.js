0f428258260fe06889b71ae11eae2fc90252ec45
Erica Haynes <ehaynes30@gmail.com>
2016-09-04 12:55:56 -0700

    Added files
    Finally added to repo

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("#js-shopping-list-form").submit(function(event){
+		event.preventDefault();
+		$('.shopping-list').append(
+			'<li>' +
+				'<span class="shopping-item">' + $("#shopping-list-entry").val() + '</span>' +
+				'<div class="shopping-item-controls">'+
+					'<button class="shopping-item-toggle">' +
+						'<span class="button-label">check</span>' +
+					'</button>' +
+					'<button class="shopping-item-delete">' +
+						'<span class="button-label">delete</span>' +
+					'</buttton>' +
+				'</div>' +
+			'</li>' 
+		);
+});
+$(".shopping-list").on("click",".shopping-item-toggle", function(event){
+	$(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+	});
+$(".shopping-list").on("click", ".shopping-item-delete", function(event){
+	$(this).closest("li").remove();
+	});	
+
+});
